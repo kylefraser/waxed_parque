05fa1143c62bbd577071b7806a1a806ac1f36c8a
Kyle Fraser <kylefraser@Kyles-MacBook-Pro.local>
2017-01-11 14:26:44 -0500

    Initial commit

---
+++ assets/js/alternate_scripts.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  $('.topbar-line').click(function(){
+    $('.topbar-line').toggleClass('topbar-line-full')
+  });
+});
---
+++ assets/js/soundcloud.js
@@ -0,0 +1,4 @@
+var app = angular.module('app', ['plangular'])
+  .config(function(plangularConfigProvider){
+    plangularConfigProvider.clientId = 'f0fcf6679bc9ed5177611ff6b891995c';
+  });
